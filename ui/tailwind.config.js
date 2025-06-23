3039df0f0b2149545d1ce5dc631ec24f763531e9
iamkaf <coffeechopdev@gmail.com>
2025-06-23 07:41:56 -0300

    improve layout and theme

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{ts,tsx}'
-  ],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'night',
+      'cyberpunk',
+      'dracula',
+      'grayscale',
+      'halloween',
+      'nightOwl',
+      'pastel',
+      'sunset',
+      'system',
+      'tango',
+      'vermilion',
+      'vivid',
+      'yeast',
+      'dark2',
+    ],
+  },
 };
